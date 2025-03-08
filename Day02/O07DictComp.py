825e04518b9ee88142929bc1b03584c2e117c666
Kiran <colkiran@gmail.com>
2025-03-08 13:06:06 +0530

    Commit

--- Day02/O07DictComp.py
+++ Day02/O07DictComp.py
@@ -9,6 +9,7 @@ print(f"sentence :{sentence}")
 words = {word: len(word) for word in sentence.split()}
 print(f"words :{words}")
 
+print("-" * 60)
 players = {
     'sachin': [350, 250, 300, 400, 385],
     'rahul': [200, 300, 450, 150, 185],
@@ -17,3 +18,39 @@ players = {
     'laxman':[345, 300, 200, 150, 190],
     'yuvraj':[190, 150, 120, 250, 275]
 }
+
+print(f"players :{players}")
+
+print("-" * 60)
+print({'sachin' : players['sachin']})
+
+print("-" * 60)
+print({'sachin' :sum(players['sachin'])})
+
+print("-" * 60)
+scores = {k: v for (k,v) in players.items()}
+print(f"scores :{scores}")
+
+print("-" * 60)
+scores = {k: sum(v) for (k, v) in players.items()}
+print(f"scores :{scores}")
+
+print("-" * 60)
+scores = {k: (lambda score: sum(score))(scores)
+          for k, scores in players.items()}
+print(f"scores :{scores}")
+
+print("-" * 60)
+scores = {k: (lambda score: sum(score) / len(score))(scores)
+          for k, scores in players.items()}
+print(f"scores :{scores}")
+
+print("-" * 60)
+scores = [score for score in players.values()]
+print(f"scores :{scores}")
+
+"""
+covert this list of list into a single list and apply a filter to print all runs >= 200
+
+a dictionary = {player_name : [runs > 200]}
+"""
