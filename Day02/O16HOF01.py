c17c998f536c836ecefecfc9de3417908d122765
Kiran <colkiran@gmail.com>
2025-03-08 15:51:09 +0530

    Commit

---
+++ Day02/O16HOF01.py
@@ -0,0 +1,32 @@
+
+def callMe():
+    print(f"Apples from Ooty")
+
+
+def fun(fnc):
+    print("Hello")
+    fnc()
+    print("hi")
+    print('_' * 40)
+
+    def defineMe():
+        print(f"Oranges from Kanpur")
+
+    return defineMe
+
+
+def funCallback(fnc):
+    print("Mera Barath Mahan")
+    fnc()
+    print("India is great")
+
+
+funCallback(fun(callMe))
+
+
+
+
+
+
+
+
